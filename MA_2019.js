0d4ed6bdcea5eff178c96be25e714cbe218890c6
Maria D. Maggio <mdmaggio7@gmail.com>
2019-07-07 02:01:31 -0400

    Update MA_2019.js

--- MA_2019.js
+++ MA_2019.js
@@ -9,38 +9,37 @@ var _TY = _StopYear = "2019"; // Tax parameters
 // 2018, 2019
 // 2019 values are not correct - just created to test
 
-_FPL[_TY] = 8880;
-_FPLAddr[_TY] = 5180;
+_FPL[_TY] = 7820;
+_FPLAddr[_TY] = 4320;
 _IndividualPercentage[_TY] = 0.0975;
 
 _AffordTab1[_TY] = [
- {incomelimit:19090, percentage:0.0},
- {incomelimit:25120, percentage:0.029},
- {incomelimit:31150, percentage:0.042},
- {incomelimit:37180, percentage:0.05},
- {incomelimit:43210, percentage:0.0745},
- {incomelimit:49240, percentage:0.076},
- {incomelimit:49241, percentage:0.0805}
+  {incomelimit:18210, percentage:0.0},
+  {incomelimit:24280, percentage:0.029},
+  {incomelimit:30350, percentage:0.042},
+  {incomelimit:36420, percentage:0.05},
+  {incomelimit:42490, percentage:0.0745},
+  {incomelimit:48560, percentage:0.076},
+  {incomelimit:48561, percentage:0.08}
 ];
 _AffordTab2[_TY] = [
- {incomelimit:25360, percentage:0.0},
- {incomelimit:31480, percentage:0.0435},
- {incomelimit:41600, percentage:0.0625},
- {incomelimit:49720, percentage:0.0745},
- {incomelimit:57840, percentage:0.0745},
- {incomelimit:65960, percentage:0.076},
- {incomelimit:65961, percentage:0.0805}
+  {incomelimit:24690, percentage:0.0},
+  {incomelimit:32920, percentage:0.043},
+  {incomelimit:41150, percentage:0.062},
+  {incomelimit:49380, percentage:0.0735},
+  {incomelimit:57610, percentage:0.0745},
+  {incomelimit:65840, percentage:0.076},
+  {incomelimit:65841, percentage:0.08}
 ];
 _AffordTab3Plus[_TY] = [
- {incomelimit:31630, percentage:0.0},
- {incomelimit:41840, percentage:0.0345},
- {incomelimit:52050, percentage:0.0495},
- {incomelimit:62260, percentage:0.0595},
- {incomelimit:72470, percentage:0.0745},
- {incomelimit:82680, percentage:0.076},
- {incomelimit:82681, percentage:0.0805}
+  {incomelimit:31170, percentage:0.0},
+  {incomelimit:41560, percentage:0.034},
+  {incomelimit:51950, percentage:0.049},
+  {incomelimit:62340, percentage:0.0585},
+  {incomelimit:72730, percentage:0.0745},
+  {incomelimit:83120, percentage:0.076},
+  {incomelimit:83121, percentage:0.08}
 ];
-
 _PremiumRegion1[_TY] = [
 
  {age:30, individual:240, couple:459, family:624},
