5d659719b3c01b23198bfa1c3c38f0526e8210fc
mdmaggio7 <51883222+mdmaggio7@users.noreply.github.com>
2024-10-25 13:45:48 -0400

    Update MA_2024.js
    fix spacing

--- MA_2024.js
+++ MA_2024.js
@@ -13,7 +13,6 @@ _FPL[_TY] = 9680;
 _FPLAddr[_TY] = 5380;
 _IndividualPercentage[_TY] = 0.0839;
 
-
 _AffordTab1[_TY] = [
     {incomelimit:21870, percentage:0},
     {incomelimit:29160, percentage:0.027},
@@ -22,8 +21,9 @@ _AffordTab1[_TY] = [
     {incomelimit:51030, percentage:0.0745},
     {incomelimit:58320, percentage:0.076},
     {incomelimit:58321, percentage:0.08}
- ];
- _AffordTab2[_TY] = [
+];
+
+_AffordTab2[_TY] = [
     {incomelimit:29580, percentage:0},
     {incomelimit:39440, percentage:0.04},
     {incomelimit:49300, percentage:0.0585},
@@ -31,8 +31,9 @@ _AffordTab1[_TY] = [
     {incomelimit:69020, percentage:0.0745},
     {incomelimit:78880, percentage:0.076},
     {incomelimit:78881, percentage:0.08}
- ];
- _AffordTab3Plus[_TY] = [
+];
+
+_AffordTab3[_TY] = [
     {incomelimit:37290, percentage:0},
     {incomelimit:49720, percentage:0.0315},
     {incomelimit:62150, percentage:0.0465},
@@ -40,10 +41,9 @@ _AffordTab1[_TY] = [
     {incomelimit:87010, percentage:0.0745},
     {incomelimit:99440, percentage:0.076},
     {incomelimit:99441, percentage:0.08}
- ];
- 
+];
 
- _PremiumRegion1[_TY] = [
+_PremiumRegion1[_TY] = [
 
     {age:30, individual:313, couple:626, family:811},
     {age:34, individual:334, couple:667, family:854},
@@ -52,9 +52,9 @@ _AffordTab1[_TY] = [
     {age:49, individual:419, couple:837, family:1023},
     {age:54, individual:486, couple:972, family:1158},
     {age:55, individual:501, couple:1001, family:1187}
- ]
+];
  
- _PremiumRegion2[_TY] = [
+_PremiumRegion2[_TY] = [
  
     {age:30, individual:299, couple:597, family:771},
     {age:34, individual:312, couple:624, family:798},
@@ -63,9 +63,9 @@ _AffordTab1[_TY] = [
     {age:49, individual:392, couple:783, family:957},
     {age:54, individual:455, couple:909, family:1083},
     {age:55, individual:468, couple:936, family:1110}
- ]
+];
  
- _PremiumRegion3[_TY] = [
+_PremiumRegion3[_TY] = [
  
     {age:30, individual:417, couple:833, family:1079},
     {age:34, individual:512, couple:1024, family:1309},
@@ -74,8 +74,7 @@ _AffordTab1[_TY] = [
     {age:49, individual:642, couple:1284, family:1570},
     {age:54, individual:746, couple:1492, family:1777},
     {age:55, individual:768, couple:1536, family:1821}
- ] 
-
+];
 
 _Region[_TY+"Berkshire"] = "1";
 _Region[_TY+"Franklin"] = "1";
